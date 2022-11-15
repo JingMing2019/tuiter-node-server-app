3fccfea7e4ad688869b5047b2392a67210357037
jingming <mary.j.ming@gmail.com>
2022-11-15 00:39:19 -0800

    complete a8 node section 5.4 implement update tuits handler

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -13,7 +13,16 @@ const createTuit = (req, res) => {
 
 const findTuits  = (req, res) => res.json(tuits);
 
-const updateTuit = (req, res) => {}
+const updateTuit = (req, res) => {
+    const tuitIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitsIndex = tuits.findIndex(tuit => tuit._id === parseInt(tuitIdToUpdate));
+    tuits[tuitsIndex] = {
+        ...tuits[tuitsIndex],
+        ...updates
+    };
+    res.sendStatus(200);
+}
 
 const deleteTuit = (req, res) => {
     const tuitIdToDelete = req.params['tid'];
