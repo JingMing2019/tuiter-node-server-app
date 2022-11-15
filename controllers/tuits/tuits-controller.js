0f4cd29d63eacf37b96e6247759ca593b02475c3
jingming <mary.j.ming@gmail.com>
2022-11-15 00:24:54 -0800

    complete a8 node section 5.2 implement create tuits handler

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -2,7 +2,15 @@ import posts from "./tuits.js";
 
 let tuits = posts;
 
-const createTuit = (req, res) => {}
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime() + '';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    tuits.push(newTuit);
+    res.json(newTuit);
+}
+
 const findTuits  = (req, res) => res.json(tuits);
 const updateTuit = (req, res) => {}
 const deleteTuit = (req, res) => {}
